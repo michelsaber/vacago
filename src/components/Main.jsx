373e3e00e4899e487360c8a72bfbf73c0d6eeeec
michelsaber <michel-saber@hotmail.com>
2024-01-06 15:59:18 +0100

    started with body

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
 import homeImage from "../images/homeImage.jpg"
 
 function Main() {
   return (
     <Container>
       <Row className='py-4'>
-        <Col sm={8} >
-            <img src={homeImage} className="rounded" alt=""/>
+        <Col sm={7} >
+            <Image src={homeImage}  fluid rounded alt=""/>
         </Col>
-        <Col sm={4} >Second, but last</Col>
+        <Col sm={5} >
+            <p className='font-italic'>Indulge your senses in the allure of sun-kissed sands and gentle waves. Our slice of paradise invites you to escape the ordinary and embrace the extraordinary. Immerse yourself in the tranquil beauty of our beach haven, where every moment is a promise of relaxation and pure bliss. Your dream vacation begins here.</p>
+        <Button variant='outline-primary'>Contact</Button>
+        </Col>
       </Row>
-      <Row className='row justify-content-md-center'> Always use the best!</Row>
+      <Card>
+      <Card.Body className='bg-dark text-white text-center' >Embark on a journey where we believe every escape deserves the best, and your extraordinary vacation starts with us.</Card.Body>
+    </Card>
     </Container>
   );
 }
